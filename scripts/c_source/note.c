7205fba6e683fec7a97e8daa4810c7a71d5d0007
d134020a18 <64816295+d134020a18@users.noreply.github.com>
2020-05-05 12:44:52 +0100

    Source

---
+++ scripts/c_source/note.c
@@ -0,0 +1,59 @@
+#include <stdio.h> //To get basic functions
+#include <string.h> //To compare strings
+#include <stdlib.h> //To get HOME dir
+int main(int argc,char * argv[]) {
+	//Check if have arguments
+	if(argv[1] != NULL) {
+		//Declare file
+		FILE *fp;
+		char line[1000];
+		//Get database file path
+		char *filepath = strcat(getenv("HOME"), "/.notes.db");
+		//Check if arg1 is "-r"
+		if(strcmp(argv[1],"-r")==0) {
+			char *pattern = argv[2];
+			//Convert pattern to syntax
+			pattern = strcat(pattern,"#");
+			//Get pattern lenght
+			int patlenght = strlen(pattern);
+			//Open file in read mode
+			fp = fopen(filepath,"r");
+			
+			//Read line by line and comparing each word in line
+			while(fscanf(fp , "%[^\n]\n" , line)!=EOF)
+           {
+				//Check if line match to search
+				if(strncmp(line, pattern, patlenght) == 0)
+				{
+					//Remove pattern of line
+					char *nop = &line[3];
+					//Print that line
+					printf(nop);
+				}
+				else
+				{
+					continue;
+				}
+           }
+			//Close file
+			fclose(fp);
+		}else{
+			char *pattern = argv[1];
+			//Open file in append mode	
+			fp = fopen (filepath,"a");
+			char comment[1000];
+			printf("Write the note:");
+			for(;;) {
+				scanf("%s", &comment);
+				if(strcmp(comment,"breaknote")!=0) {
+					//Save to file
+					fprintf(fp, "%s#%s\n", pattern,comment);
+					//If read breaknote then break
+				}else{
+					break;
+				}
+			}
+		}
+	return 0;
+	}
+}
