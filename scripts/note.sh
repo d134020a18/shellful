43b54eda1a0bdf79ac89e55998f9ed2e265266a2
d134020a18 <64816295+d134020a18@users.noreply.github.com>
2020-05-07 22:47:39 +0100

    Update note.sh

--- scripts/note.sh
+++ scripts/note.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+#Check arguments
 if [ "$1" == "-r" ]; then
+#Get note content in database that match with name
 cat ~/.notes.db | grep "\#$2" | sed -e "s/#$2//"
 else
 	echo Write the note:
+	#Do a loop
 	for (( ; ; )); do
 		read note;
+		#Break if detect entered breaknote
 		if [ $note == breaknote ]; then
 			break;
 		else
+			#Add the content to database
 			echo $note\#$1 >> ~/.notes.db
 		fi;
 	done
